70f792e8ce3d3b9a03c6e1743929edd37a4bb81b
Ryan Ward <rayaman99@gmail.com>
2023-04-29 12:12:16 -0400

    Testing

--- src/main.c
+++ src/main.c
@@ -59,7 +59,7 @@ int gettimeofday(struct timeval * tp, struct timezone * tzp)
 }
 
 static long get_timestamp() {
-    struct timeval tv;
+    timeval tv;
     gettimeofday(&tv, NULL);
 
     return tv.tv_sec * 1000 + tv.tv_usec / 1000;
