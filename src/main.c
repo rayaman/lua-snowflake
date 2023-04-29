553f9789a45908155f1541ca750eaf7a49a172e2
Ryan Ward <rayaman99@gmail.com>
2023-04-29 12:21:02 -0400

    Testing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <lua.h>
 #include <lauxlib.h>
 #include <stdbool.h>
-#include <windows.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 #if LUA_VERSION_NUM < 502
@@ -27,41 +31,26 @@ static int g_sequence = 0;
 
 #define SEQUENCE_MASK (0xffffffff ^ (0xffffffff << SEQUENCE_BITS))
 
-#define WIN32_LEAN_AND_MEAN
-#include <stdint.h> // portable: uint64_t   MSVC: __int64 
+static uint64_t get_timestamp() {
+    // LARGE_INTEGER frequency;        // ticks per second
+    // LARGE_INTEGER t1;
+    // QueryPerformanceFrequency(&frequency);
+    // QueryPerformanceCounter(&t1);
 
-// MSVC defines this in winsock2.h!?
-typedef struct timeval {
-    long tv_sec;
-    long tv_usec;
-} timeval;
-
-int gettimeofday(struct timeval * tp, struct timezone * tzp)
-{
-    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
-    // until 00:00:00 January 1, 1970 
-    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
-
-    SYSTEMTIME  system_time;
-    FILETIME    file_time;
-    uint64_t    time;
-
-    GetSystemTime( &system_time );
-    SystemTimeToFileTime( &system_time, &file_time );
-    time =  ((uint64_t)file_time.dwLowDateTime )      ;
-    time += ((uint64_t)file_time.dwHighDateTime) << 32;
-
-    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
-    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
-    return 0;
+    // return t1.QuadPart * 1000.0 / frequency.QuadPart;
+    return time_ns();
 }
 
-static long get_timestamp() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
+static uint64_t time_ns(void)
+{
+    struct timespec ts;
 
-    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    if (timespec_get(&ts, TIME_UTC) != TIME_UTC)
+    {
+        fputs("timespec_get failed!", stderr);
+        return 0;
+    }
+    return 1000000000 * ts.tv_sec + ts.tv_nsec;
 }
 
 static long get_til_next_millis(long last_timestamp) {
